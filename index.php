d801c96f6951f9033ef5926aa741aca2ef3f6b50
Jeremyofishail <79768101+Jeremyofishail@users.noreply.github.com>
2022-09-14 15:07:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Welcome to the game hub!</p>'; ?> <?php echo '<p>https://classic.minecraft.net/</p>'; ?> '<p>http://slither.io/</p> 
+
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
+</image>
